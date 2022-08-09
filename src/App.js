b0dcd2431c1884b5d2c88746fb0fba85ca09512f
Shaikh Hina <hinashaikh53@gmail.com>
2022-08-09 23:04:45 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
+
+import "./style.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const itemList = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Java",
+    "Python",
+    "PHP",
+    "MYSQL",
+    "Oracle"
+  ];
+ 
+  const [filteredList, setFilteredList] = new useState(itemList);
+
+  const filterBySearch = (event) => {
+    // Access input value
+    const query = event.target.value;
+    // Create copy of item list
+    var updatedList = [...itemList];
+    // Include all elements which includes the search query
+    updatedList = updatedList.filter((item) => {
+      return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    // Trigger render with updated values
+    setFilteredList(updatedList);
+  };
+
+  return (
+    <div className="App">
+       <h1><i>Coding Languages</i></h1>
+      <div className="search-header">
+     
+        <input id="search-box" placeholder="Search here" onChange={filterBySearch} />
+      </div>
+      <div id="item-list">
+        <ol>
+          {filteredList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
